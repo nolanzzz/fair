029f402a2adbfdf90b70999303d40c6f3991fc10
zhanr110 <nolanwork131@gmail.com>
2021-05-30 23:46:16 -0400

    create gen data path function for mta

--- src/gen_data_path.py
+++ src/gen_data_path.py
@@ -68,6 +68,24 @@ def gen_data_path_mot17_emb(root_path):
     f.close()
 
 
+def gen_data_path_mta_train(root_path):
+    mta_path = 'data/MTA/mta_data/images/train'
+    real_path = os.path.join(root_path, mta_path)
+    write_file = os.path.join(root_path, 'src/data/mta.train')
+    seq_names = [s for s in sorted(os.listdir(real_path))]
+    # with open(write_file, 'w') as f:
+    #     for seq_name in seq_names:
+    #         seq_path = os.path.join(real_path, seq_name)
+    #         seq_path = os.path.join(seq_path, 'img1')
+    #         images = sorted(glob.glob(seq_path + '/*.jpg'))
+    #         len_all = len(images)
+    #         # len_half = int(len_all / 2)
+    #         for i in range(len_all):
+    #             image = images[i]
+    #             print(image[22:], file=f)
+    # f.close()
+
+
 if __name__ == '__main__':
-    root = '/data/MOT/JDE'
-    gen_data_path_mot17_emb(root)
+    root = '/u40/zhanr110/fair-tracker'
+    gen_data_path_mta_train(root)
