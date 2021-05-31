54751ae56b74d4efff179cee6b2075401cb3dcab
zhanr110 <nolanwork131@gmail.com>
2021-05-31 16:16:39 -0400

    change names in sort_gt

--- utils/sort_gt.py
+++ utils/sort_gt.py
@@ -5,8 +5,8 @@ data_type = sys.argv[1]
 # cam_#
 cam = sys.argv[2]
 directory = '../data/MTA/mta_data/images/' + data_type + '/' + cam + '/gt'
-lines = open(directory + '/gt.txt', 'r').readlines()
-output = open(directory + '/sorted.txt', 'w')
+lines = open(directory + '/unsorted.txt', 'r').readlines()
+output = open(directory + '/gt.txt', 'w')
 for line in sorted(lines, key=lambda l: int(l.split(',')[1]), reverse=False):
     output.write(line)
 output.close()
