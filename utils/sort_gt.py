67eca3338ec4cb517deb1bcceca3e345ea30a380
zhanr110 <nolanwork131@gmail.com>
2021-05-31 16:06:46 -0400

    remove test

--- utils/sort_gt.py
+++ utils/sort_gt.py
@@ -5,8 +5,6 @@ data_type = sys.argv[1]
 # cam_#
 cam = sys.argv[2]
 directory = '../data/MTA/mta_data/images/' + data_type + '/' + cam + '/gt'
-print(directory + '/gt.txt')
-print(directory + '/sorted.txt')
 lines = open(directory + '/gt.txt', 'r').readlines()
 output = open(directory + '/sorted.txt', 'w')
 for line in sorted(lines, key=lambda l: int(l.split(',')[1]), reverse=False):
