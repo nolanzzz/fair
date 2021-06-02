b67bbc6d389f7a0a85c44031f7f420ab30097a01
zhanr110 <nolanwork131@gmail.com>
2021-06-02 01:31:49 -0400

    update cuda

--- src/train.py
+++ src/train.py
@@ -93,6 +93,6 @@ def main(opt):
 
 
 if __name__ == '__main__':
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0, 1'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
     opt = opts().parse()
     main(opt)
