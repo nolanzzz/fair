45eade4618869aec9aaf126b2ea8cd1b702252d0
zhanr110 <nolanwork131@gmail.com>
2021-06-01 23:24:50 -0400

    update mta_dla sh

--- experiments/mta_dla34.sh
+++ experiments/mta_dla34.sh
@@ -1,3 +1,3 @@
 cd src
-python train.py mot --exp_id mta_dla34 --load_model '../exp/mot/mot17_dla34/model_last.pth' --data_cfg '../src/lib/cfg/mta.json'
+CUDA_VISIBLE_DEVICES="1, 3" python train.py mot --exp_id mta_dla34 --load_model '../exp/mot/mot17_dla34/model_last.pth' --data_cfg '../src/lib/cfg/mta.json' --gpus 1,3
 cd ..
