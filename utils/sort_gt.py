00239ea485e2846d608c6cc504a2a7e1f8c4e2bf
zhanr110 <nolanwork131@gmail.com>
2021-06-04 11:31:53 -0400

    update sort_gt

--- utils/sort_gt.py
+++ utils/sort_gt.py
@@ -2,9 +2,9 @@ import sys
 
 # train or test
 data_type = sys.argv[1]
-# cam_#
-cam = sys.argv[2]
-directory = '../data/MTA/mta_data/images/' + data_type + '/' + cam + '/gt'
+# cam #
+folder_no = sys.argv[2]
+directory = '../data/MTA/mta_data/images/' + data_type + '/cam_' + folder_no + '/gt'
 lines = open(directory + '/unsorted.txt', 'r').readlines()
 output = open(directory + '/gt.txt', 'w')
 for line in sorted(lines, key=lambda l: int(l.split(',')[1]), reverse=False):
