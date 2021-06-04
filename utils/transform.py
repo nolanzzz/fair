29f92c8280b66203bdf9a36d18e96fe253fcb5fa
zhanr110 <nolanwork131@gmail.com>
2021-06-04 11:45:26 -0400

    txt to csv in transform

--- utils/transform.py
+++ utils/transform.py
@@ -6,7 +6,7 @@ import numpy as np
 data_type = sys.argv[1]
 folder_no = sys.argv[2]
 path = '/u40/zhanr110/MTMCT/data/MTA/mta_data/images/' + data_type + '/cam_' + folder_no
-filename = path + '/coords_fib_cam_' + folder_no + '.txt'
+filename = path + '/coords_fib_cam_' + folder_no + '.csv'
 
 tid_curr = 0
 tid_last = -1
