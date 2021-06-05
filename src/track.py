7558c0297e98fa14446ca5a308a7e4a6807551b5
zhanr110 <nolanwork131@gmail.com>
2021-06-05 13:57:44 -0400

    add partial to test_mta

--- src/track.py
+++ src/track.py
@@ -208,12 +208,12 @@ if __name__ == '__main__':
                       Venice-1'''
         data_root = os.path.join(opt.data_dir, 'MOT15/images/test')
     if opt.test_mta:
-        seqs_str = '''cam_0
-                      cam_1
-                      cam_2
-                      cam_3
-                      cam_4
-                      cam_5'''
+        seqs_str = '''cam_0/partial
+                      cam_1/partial
+                      cam_2/partial
+                      cam_3/partial
+                      cam_4/partial
+                      cam_5/partial'''
         data_root = os.path.join(opt.data_dir, 'mta_data/images/test')
     if opt.test_mot17:
         seqs_str = '''MOT17-01-SDP
