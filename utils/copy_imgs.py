9e51389f8295c3d177420c41f429f293764e3f76
zhanr110 <nolanwork131@gmail.com>
2021-06-05 15:58:15 -0400

    allow argv frames for copy img

--- utils/copy_imgs.py
+++ utils/copy_imgs.py
@@ -1,12 +1,15 @@
 import shutil
+import sys
 
+
+frames = int(sys.argv[1])
 for i in range(6):
     j, period_i, curr_step = 1, 0, 0
     while j <= 127880:
         if curr_step == 5:
             break
         # enough frames for current step
-        if period_i > 1000:
+        if period_i > frames:
             curr_step += 1
             period_i = 0
             j = j + 25000
