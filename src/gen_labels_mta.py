49ccbb2e039323b5ae84f3232ee4391760dde57e
zhanr110 <nolanwork131@gmail.com>
2021-06-13 16:50:43 -0400

    update gen_labels for mta data - use consistent identities over cams

--- src/gen_labels_mta.py
+++ src/gen_labels_mta.py
@@ -16,8 +16,6 @@ label_root = '../data/MTA/mta_data/labels_with_ids/' + data_type
 mkdirs(label_root)
 seqs = [s for s in os.listdir(seq_root)]
 
-tid_curr = 0
-tid_last = -1
 for seq in seqs:
     seq_info = open(osp.join(seq_root, seq, 'seqinfo.ini')).read()
     seq_width = int(seq_info[seq_info.find('imWidth=') + 8:seq_info.find('\nimHeight')])
@@ -30,17 +28,12 @@ for seq in seqs:
     mkdirs(seq_label_root)
 
     for fid, tid, x, y, w, h, mark, label, _ in gt:
-        if mark == 0 or not label == 1:
-            continue
         fid = int(fid)
         tid = int(tid)
-        if not tid == tid_last:
-            tid_curr += 1
-            tid_last = tid
         x += w / 2
         y += h / 2
         label_fpath = osp.join(seq_label_root, '{:06d}.txt'.format(fid))
         label_str = '0 {:d} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(
-            tid_curr, x / seq_width, y / seq_height, w / seq_width, h / seq_height)
+            tid, x / seq_width, y / seq_height, w / seq_width, h / seq_height)
         with open(label_fpath, 'a') as f:
             f.write(label_str)
